6b04b716f9522531f6c27fba3d988b92bc96b219
unknown <polibos6366@gmail.com>
2024-09-20 17:25:58 -0600

    NOT NULL, UNIQUE, PRIMARY KEY, AUTO_INCREMENT, CHECK

--- Tables/01_create_table.sql
+++ Tables/01_create_table.sql
@@ -5,3 +5,47 @@ age int,
 email varchar(50),
 created date
 )
+
+
+CREATE TABLE persons2 (
+id int not null,
+name varchar(100) not null, 
+age int,
+email varchar(50),
+created date
+)
+
+
+CREATE TABLE persons3 (
+id int NOT NULL,
+name varchar(100) NOT NULL, 
+age int,
+email varchar(50),
+created datetime,
+UNIQUE(id)
+)
+
+CREATE TABLE persons4 (
+id int NOT NULL PRIMARY KEY UNIQUE,
+name varchar(100) NOT NULL, 
+age int,
+email varchar(50),
+created datetime
+)
+
+CREATE TABLE persons5 (
+id int NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
+name varchar(100) NOT NULL, 
+age int,
+email varchar(50),
+created datetime
+)
+
+CREATE TABLE persons6 (
+id int NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
+name varchar(100) NOT NULL, 
+age int,
+email varchar(50),
+created datetime,
+CHECK(age >= 18)
+)
