d86f78c73efb454daf031bd60781fe8b639fd2aa
unknown <polibos6366@gmail.com>
2024-09-20 16:48:20 -0600

    INSERT INTO

---
+++ Database/01_insert_into.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (user_id,name,surname) VALUES (8, 'Ricardo', 'Lopez');
+
+
+INSERT INTO users (name,surname) VALUES ( 'Paco', 'Perez');
+
--- Statements/08_null.sql
+++ Statements/08_null.sql
@@ -3,3 +3,5 @@ SELECT * FROM users WHERE email IS NULL;
 SELECT * FROM users WHERE email IS NOT NULL;
 
 SELECT * FROM users WHERE email IS NOT NULL;
+
+SELECT name,surname, IFNULL(age,0) AS age FROM users  
