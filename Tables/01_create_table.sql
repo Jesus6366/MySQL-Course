30bfac93f8f6f281ec345b4632b47904a8f0d5bd
unknown <polibos6366@gmail.com>
2024-09-20 17:30:44 -0600

    DROP TABLE

--- Tables/01_create_table.sql
+++ Tables/01_create_table.sql
@@ -49,3 +49,12 @@ email varchar(50),
 created datetime,
 CHECK(age >= 18)
 )
+
+CREATE TABLE persons7 (
+id int NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
+name varchar(100) NOT NULL, 
+age int,
+email varchar(50),
+created datetime DEFAULT CURRENT_TIMESTAMP(),
+CHECK(age >= 18)
+)
