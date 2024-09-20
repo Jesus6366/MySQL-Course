d1835ba89914d60603ac81a30a75611a3e132a32
unknown <polibos6366@gmail.com>
2024-09-19 20:41:43 -0600

    AS statement

---
+++ Statements/15_alias.sql
@@ -0,0 +1,4 @@
+SELECT name, init_date AS 'initial date of programming' FROM users WHERE age BETWEEN 20 AND 33
+
+
+SELECT name, init_date AS 'initial date of programming' FROM users WHERE name = 'brais'
