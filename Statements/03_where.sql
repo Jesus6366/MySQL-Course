0f4f1e172a0318fbc6af4acd6b11e68d174f2162
unknown <polibos6366@gmail.com>
2024-09-19 19:47:16 -0600

    ORDER BY statement

--- Statements/03_where.sql
+++ Statements/03_where.sql
@@ -1,3 +1,7 @@
 SELECT * FROM users WHERE age = 20;
 
 SELECT * FROM users WHERE name = "jesus"
+
+SELECT name FROM users WHERE age = 20;
+
+SELECT distinct age FROM users WHERE age = 20;
---
+++ Statements/04_order_by.sql
@@ -0,0 +1,8 @@
+SELECT * FROM users ORDER BY age;
+
+SELECT * FROM users ORDER BY age ASC;
+
+SELECT * FROM users ORDER BY age DESC;
+
+
+SELECT name FROM users WHERE email = "sara@gmail.com" ORDER BY age DESC;
